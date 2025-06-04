d75d035229a768285eb3d826aec8c63dc2bad1ad
kduran08 <kduran50480@ufide.ac.cr>
2025-06-04 14:39:06 -0600

    Inventario actualizado
    Se crea el producto model, inventario controller y sus vistas.

---
+++ prototipo2/prototipo2/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prototipo2.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public required string Nombre_Producto { get; set; }
+        [Required(ErrorMessage = "La categoria es obligatoria")]
+        public required string Categoria { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "La cantidad no puede ser negativa")]
+        public int Cantidad { get; set; }
+        [Required(ErrorMessage = "Ingrese el nombre del proveedor")]
+        public required string Proveedor { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que cero")]
+        public required decimal Precio { get; set; }
+    }
+}

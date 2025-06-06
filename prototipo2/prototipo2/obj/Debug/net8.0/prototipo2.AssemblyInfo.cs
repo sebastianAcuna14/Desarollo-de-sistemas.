ee8d03f0e45d187f89eb988621f79fbb4d590e81
JoshuaMeji <156728872+JoshuaMeji@users.noreply.github.com>
2025-06-05 22:05:56 -0600

    Arreglo Cabeza

--- prototipo2/prototipo2/obj/Debug/net8.0/prototipo2.AssemblyInfo.cs
+++ prototipo2/prototipo2/obj/Debug/net8.0/prototipo2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1151e6fed78cbfa2d3c6226f45e692cc0c32a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4f7b3d9b542c13c3f85b9fc809178db18d0b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

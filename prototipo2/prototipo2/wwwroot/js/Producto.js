848dfde45f8d2b6c1f49adc6e66fbb04786d95b2
kduran08 <kduran50480@ufide.ac.cr>
2025-06-06 19:46:11 -0600

    Actualizacion del CRUD Inventario
    Se hacen cambios de diseño en todas las vistas de la carpeta Inventario, se añade un JavaScript Producto y el uso de SweetAlert2 para stock <= 2

---
+++ prototipo2/prototipo2/wwwroot/js/Producto.js
@@ -0,0 +1,15 @@
+﻿//Para el datatable de productos
+document.addEventListener('DOMContentLoaded', function () {
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple, {
+            perPage: 10,
+            labels: {
+                placeholder: "Buscar Producto...",
+                noRows: "No se encontraron Productos",
+                info: "Mostrando {start} a {end} de {rows} productos",
+            }
+            // No definir columnas si no hay columnas especiales como fechas
+        });
+    }
+});

af4a31022cf3db46dba8b08166d1476eda3c389c
JoshuaMeji <156728872+JoshuaMeji@users.noreply.github.com>
2025-07-24 20:01:08 -0600

    Decoracion Final

--- prototipo2/prototipo2/obj/Debug/net8.0/prototipo2.AssemblyInfo.cs
+++ prototipo2/prototipo2/obj/Debug/net8.0/prototipo2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6387fd724ab947bf410fbe5bce2629c358ecf44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9f682a11461b96f4557871757a8530abc9068c")]
 [assembly: System.Reflection.AssemblyProductAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
